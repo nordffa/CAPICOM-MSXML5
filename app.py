4e0c41f8bcd82b8edadcdfc424f940adf8dd2503
nordffa <nordffa@gmail.com>
2024-10-08 19:16:46 -0300

    08-10-2024

--- app.py
+++ app.py
@@ -13,3 +13,24 @@ def copiar_e_registrar_dll(nome_dll, pasta_destino):
 
     subprocess.run(["regsvr32", "/s", caminho], check=True)
     print(f'O arquivo {nome_dll} foi registrado')
+
+def main():
+    sistema_64x = os.path.join(os.environ['WINDIR'], "SysWOW64")
+    sistema_32x = os.path.join(os.environ['WINDIR'], "System32")
+
+    if os.path.exists(sistema_64x):
+        pasta_destino_dll = sistema_64x
+        print("*** Sistema 64 bits detectado ***")
+    else:
+        pasta_destino_dll = sistema_32x
+        print("*** Sistema 32 bits detectado ***")
+
+    dlls = ["capicom.dll", "msxml5.dll", "msxml5r.dll"]
+    for dll in dlls:
+        copiar_e_registrar_dll(dll, pasta_destino_dll)
+    
+    print("*** CAPICOM e MSXML5 instalados ***")
+    os.system("PAUSE")
+
+if __name__ == "__main__":
+    main()
