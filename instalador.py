f0647e5caec798f4c6d93386fb03a0166eec7ea4
nordffa <nordffa@gmail.com>
2024-10-08 22:47:51 -0300

    08-10-2024

--- instalador.py
+++ instalador.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 
-def copiar_dll(nome_dll, pasta_destino):
+def copiar_e_registrar_dll(nome_dll, pasta_destino):
     caminho = os.path.join(pasta_destino, nome_dll)
 
     if not os.path.exists(caminho):
@@ -11,9 +11,6 @@ def copiar_dll(nome_dll, pasta_destino):
     else:
         print(f'O arquivo {nome_dll} ja existe na pasta {pasta_destino}')
 
-
-def registrar_dll(nome_dll, pasta_destino):
-    caminho = os.path.join(pasta_destino, nome_dll)
     os.system("regsvr32 " + caminho + " /s")
     print(f'O arquivo {nome_dll} foi registrado\n')
 
@@ -31,11 +28,7 @@ def main():
 
     dlls = ["capicom.dll", "msxml5.dll", "msxml5r.dll"]
     for dll in dlls:
-        copiar_dll(dll, pasta_destino_dll)
-        if dll != "msxml5r.dll":
-            registrar_dll(dll, pasta_destino_dll)
-        else:
-            print(f'O arquivo {dll} nao precisa de registro')
+        copiar_e_registrar_dll(dll, pasta_destino_dll)
 
     print('\n*** CAPICOM e MSXML5 instalados ***\n')
     os.system("PAUSE")
